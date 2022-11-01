6043f79a42e77c2bf59f26d73bcf465b6bc07789
Zander <alexander.backlin.salas@iths.se>
2022-11-01 09:21:22 +0100

    gjorde en filehandler fil

---
+++ Labb_3_WPF/Filehandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3_WPF
+{
+    public class Filehandler
+    {
+
+        public static void WriteFile(string text)
+        {
+
+            using (StreamWriter writeOrder = new StreamWriter("bokningar.log", true))
+            {
+
+                writeOrder.WriteLine(text);
+            }
+        }
+
+        
+        
+        public async Task<List<string>> GetTextsFile()
+        {
+            if (File.Exists("bokningar.log") == false)
+            {
+                WriteFile("Bokningar 14 november till 20 november");
+            }
+
+
+            List<string> texts = new List<string>();
+
+
+            string line = "";
+
+
+
+            using (StreamReader stream = new StreamReader("bokningar.log"))
+            {
+                while ((line = stream.ReadLine()) != null)
+                {
+                    texts.Add(line);
+
+                }
+            }
+
+            return texts;
+
+        }
+    }
+}
--- Labb_3_WPF/MainWindow.xaml.cs
+++ Labb_3_WPF/MainWindow.xaml.cs
@@ -130,43 +130,6 @@ namespace Labb_3_WPF
 
 
 
-        public static void WriteFile(string text)
-        {
-
-            using (StreamWriter writeOrder = new StreamWriter("bokningar.log", true))
-            {
-
-                writeOrder.WriteLine(text);
-            }
-        }
-
-        public static List<string> GetTextsFile()
-        {
-            if (File.Exists("bokningar.log") == false)
-            {
-                WriteFile("Bokningar 14 november till 20 november");
-            }
-
-
-            List<string> texts = new List<string>();
-
-
-            string line = "";
-
-
-
-            using (StreamReader stream = new StreamReader("bokningar.log"))
-            {
-                while ((line = stream.ReadLine()) != null)
-                {
-                    texts.Add(line);
-
-                }
-            }
-
-            return texts;
-
-        }
 
 
 
